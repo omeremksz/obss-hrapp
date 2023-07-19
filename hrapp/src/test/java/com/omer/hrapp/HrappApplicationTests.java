765d0a04803d05b9869c52f90e6759f9aa302133
Ömer Emeksiz <omeremksz@gmail.com>
2023-07-19 19:21:32 +0300

    omer added main project

---
+++ hrapp/src/test/java/com/omer/hrapp/HrappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omer.hrapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
